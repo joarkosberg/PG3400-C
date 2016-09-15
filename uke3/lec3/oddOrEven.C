37b782a3a3e2be8a0bb9d53793c41f76233f4e2b
Joar Kosberg <joar_kosberg@hotmail.com>
2016-09-15 12:15:21 +0100

    Exercise 3 complete, program checks if input is odd or even

--- uke3/lec3/oddOrEven.C
+++ uke3/lec3/oddOrEven.C
@@ -1,11 +1,20 @@
 #include <stdio.h>
+#include <stdlib.h>
 int main(int argc, char *argv[])
 {
 	printf("Odd or Even?\n");
 
-	
+	int number = atoi(argv[1]);
 
-
-
+	if(number == 0){
+		printf("Please provide a non-zero integer as argument!\n");
+	} else {
+		if(number % 2 == 0){
+			printf("Number %d is read and it is even", number);
+		} else {
+			printf("Number %d is read and it is odd", number);
+		}
+		printf("\n");
+	}
 	return 0;
 }
