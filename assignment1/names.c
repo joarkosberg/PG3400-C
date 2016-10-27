3f04aaed62e51190c75c2b61a8623f65af4d972f
Joar Kosberg <joar_kosberg@hotmail.com>
2016-10-27 07:37:28 +0100

    Added work on assignment

--- assignment1/names.c
+++ assignment1/names.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 void swap(int *array, int a, int b);
 
@@ -7,22 +8,32 @@ int main (){
 	//Welcome message
 	printf("Welcome to word sort!\n");
 
-	//Init resources
-	int capacity = 10;
-	int size = 0;
-	int *numbers = malloc(capacity*sizeof(int));
 
-	//Get input
-	int input;
-	while(1){
-		scanf("%d", &input);
-		if(input == 0) break;
-		numbers[size++] = input;
-		if(size == capacity){
-			capacity = capacity * 1.6;
-			numbers = realloc(numbers, capacity*sizeof(int));
-		}
-	}
+/*
+Gather:
+With this assignment you will find some files with individual names on lines.
+Write a program that takes a filename as input and reads the names from the file
+until the end of file. This program should store the names in an array and also
+the count in a variable. To store variable length names, you can either use an
+array of pointers or a 2D array with a reasonable maximum length for each
+name.
+
+Example command line:
+./myprogram human_male.txt
+Execution of this must read all the integers in human_male.txt
+*/
+
+	//Get user input
+	int length = 30;
+	char *file = malloc(length*sizeof(char));
+
+	scanf("%s", file);
+	printf("File to read: %s\n", file);
+
+	//Get words from file
+
+
+/*
 
 	//Sort!
 	int done = 1;
@@ -35,14 +46,26 @@ int main (){
 			}
 		}
 	} while (!done);
+*/
 
 
+
+/*
+Sorting:
+Once the names are read into the memory, you should sort them alphabetically,
+preferably without copying to a new location. You should already have learned
+Bubble Sort. After sorting the program should print all the names.
+*/
+
+
+/*
 	//Print
 	printf("You sorted %d numbers:\n", size);
 	for(int k = 0; k < size; k++){
 		printf("%d - ", numbers[k]);
 	}
 	printf("\n");
+*/
 }
 
 void swap(int *array, int a, int b){
